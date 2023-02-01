ae8c7432bbad9544d346f9cc04eccc796e97e7ed
KatiaProkina <prokina.katia@gmail.com>
2023-02-01 22:32:15 +0300

    pixel perfect,checkbox,logic of counting products

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 document.addEventListener('DOMContentLoaded',function(){
     
-    const form = document.querySelector('.form')
     const formOrder = document.querySelector('.order-form')
-    const btn = document.querySelector('.order-button')
     const name = document.querySelector('.name')
     const secondName = document.querySelector('.second_name')
     const email = document.getElementById('email')
@@ -15,9 +13,100 @@ document.addEventListener('DOMContentLoaded',function(){
     let missitems = document.querySelector('.missing-products')
     let items = document.querySelector('.items-in-the-cart')
 
-  
+ 
 
+    let calculate = document.getElementById("calculation");
+    let count = document.getElementById("buttonCountNumber")
+    let calculation = document.getElementById("calculation").innerHTML;
+
+   
+
+     document.getElementById("buttonCountPlus").onclick = function() {
+      let countPlus = count.innerHTML;
+      if(countPlus <= 1){
+        count.innerHTML++;
+        let countPlus = count.innerHTML;
+        calculate.innerHTML = calculations(countPlus) ;
+  }
+ 
+}
+    document.getElementById("buttonCountMinus").onclick = function() {
+      let countMinus = count.innerHTML;
+      if(+countMinus >= 2){
+        count.innerHTML--;
+        let countMinus = count.innerHTML;
+        calculate.innerHTML = calculations(countMinus) ;
+  }
   
+}
+
+ calculations = (count) => {
+  return (+count)*(+calculation);
+}
+
+
+let calculate2 = document.getElementById("calculation-2");
+let count2 = document.getElementById("buttonCountNumber-2")
+let calculation2 = document.getElementById("calculation-2").innerHTML;
+
+
+calculations2 = (count2) => {
+  return (+count2)*(+calculation2);
+}
+
+document.getElementById("buttonCountPlus-2").onclick = function() {
+  let countPlus2 = count2.innerHTML;
+  if(countPlus2 <= 1){
+    count2.innerHTML++;
+    let countPlus2 = count2.innerHTML;
+    calculate2.innerHTML = calculations2(countPlus2) 
+}
+
+}
+document.getElementById("buttonCountMinus-2").onclick = function() {
+  let countMinus2 = count2.innerHTML;
+  if(+countMinus2 >= 2){
+    count2.innerHTML--
+    let countMinus2 = count2.innerHTML;
+    calculate2.innerHTML = calculations2(countMinus2)
+}
+}
+
+let calculate3 = document.getElementById("calculation-3");
+let count3 = document.getElementById("buttonCountNumber-3")
+let calculation3 = Number(document.getElementById("calculation-3").innerHTML);
+
+
+
+calculations3 = (count3) => {
+  return (calculation3)*(count3);
+}
+
+
+document.getElementById("buttonCountPlus-3").onclick = function() {
+  let countPlus3 = count3.innerHTML;
+  if(countPlus3 <= 300){
+    count3.innerHTML++;
+    let countPlus3 = count3.innerHTML;
+    calculate3.innerHTML = calculations3(countPlus3) 
+}
+
+}
+document.getElementById("buttonCountMinus-3").onclick = function() {
+  let countMinus3 = count3.innerHTML;
+  if(countMinus3 >= 1){
+    count3.innerHTML--;
+    let countMinus3 = count3.innerHTML;
+    calculate3.innerHTML = calculations3(countMinus3)
+}
+}
+
+let totalPrice = document.getElementById('total-price')
+
+
+
+
+
     
     function createError(input,text){
         const parent = input.parentNode
